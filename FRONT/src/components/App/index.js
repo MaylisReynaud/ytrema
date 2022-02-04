65f592e668d80d6ab59aad8f7aa8bb7271a0afad
MaylisReynaud <maylis.reynaud74@gmail.com>
2022-02-04 11:12:19 +0100

    starting project

---
+++ FRONT/src/components/App/index.js
@@ -0,0 +1,17 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <img src={reactLogo} alt="react logo" />
+    <h1>Composant : App</h1>
+  </div>
+);
+
+// == Export
+export default App;
