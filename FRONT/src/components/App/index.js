2eaabb230ed07cc273b568870b3bbeb1cab05dd3
MaylisReynaud <maylis.reynaud74@gmail.com>
2022-02-04 11:41:36 +0100

    clear project and start with ytrema elements

--- FRONT/src/components/App/index.js
+++ FRONT/src/components/App/index.js
@@ -2,14 +2,14 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
+import ytrema from '/home/maylis/ytrema/FRONT/src/assets/ytrema.png';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <img src={ytrema} alt="ÿtrema logo" />
+    <h1>Bienvenue sur ÿ tréma</h1>
   </div>
 );
 
---
+++ README.md
@@ -0,0 +1,4 @@
+💛 "ÿ tréma" is design for seamstresses by a seamstress ! 💛
+Organize your fabrics, haberdashery, patterns and and keep an eye 👀 on what's left in stock to create your project. And let's save what you do, what you use in each project.
+With "ÿ tréma" you will always have all the information at your fingertips !
+
