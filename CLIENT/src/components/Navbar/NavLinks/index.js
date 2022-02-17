1841b27bbfc4be79f4b7c9576504b015cf2c9a2d
MaylisReynaud <maylis.reynaud74@gmail.com>
2022-02-17 17:07:54 +0100

    navbar ADD icon on desktop menu

--- CLIENT/src/components/Navbar/NavLinks/index.js
+++ CLIENT/src/components/Navbar/NavLinks/index.js
@@ -4,6 +4,19 @@ import { NavLinksContainer,
          LinkItem,
          Link
  } from './style';  
+ import tissuImg from '../../../assets/images/tissu-noir.png';
+ import tissuImgColor from '../../../assets/images/tissu-jaune.png';
+ import boutonImg from '../../../assets/images/bouton-noir.png';
+ import boutonImgColor from '../../../assets/images/bouton-vert.png';
+ import patronImg from '../../../assets/images/patron-noir.png';
+ import patronImgColor from '../../../assets/images/patron-vert.png';
+ import projetImg from '../../../assets/images/mac-noir.png';
+ import projetImgColor from '../../../assets/images/mac-orange.png';
+ 
+
+
+
+
 
 
 export function NavLinks(props) {
@@ -11,16 +24,28 @@ export function NavLinks(props) {
     <NavLinksContainer>
       <LinksWrapper>
           <LinkItem>
-            <Link href="/tissus">Tissus</Link>
+            <Link href="/tissus">
+            <img src={tissuImg}></img>
+              Tissus
+            </Link>
           </LinkItem>
           <LinkItem>
-            <Link href="/mercerie">Mercerie</Link>
+            <Link href="/mercerie">
+            <img src={boutonImg}></img>
+              Mercerie
+            </Link>
           </LinkItem>
           <LinkItem>
-            <Link href="/patrons">Patrons</Link>
+            <Link href="/patrons">
+            <img src={patronImg}></img>
+              Patrons
+            </Link>
           </LinkItem>
           <LinkItem>
-            <Link href="/projets ">Projets</Link>
+            <Link href="/projets ">
+            <img src={projetImg}></img>
+              Projets
+            </Link>
           </LinkItem>
         </LinksWrapper>
     </NavLinksContainer>
--- CLIENT/src/components/Navbar/NavLinks/style.js
+++ CLIENT/src/components/Navbar/NavLinks/style.js
@@ -10,29 +10,43 @@ export const LinksWrapper = styled.ul`
   margin: 0;
   padding: 0;
   display: flex;
+  align-items: center;
   height: 100%;
   list-style: none;
 `;
 
+
+
 export const LinkItem = styled.li`
   height: 100%;
-  padding: 0 1.1em;
+  margin: 0 2.2em;
+  
   color: #222;
   font-weight: 500;
   font-size: 14px;
   align-items: center;
-  justify-content: center;
+  /* justify-content: center; */
   display: flex;
   border-top: 2px solid transparent;
   transition: all 220ms ease-in-out;
   &:hover {
-    border-top: 2px solid #ffc43d;
+    border-bottom: 2px solid #ffc43d;
     color: #ffc43d;
+    padding: 0 1.5em;
   }
 `;
 
 export const Link = styled.a`
   text-decoration: none;
+  margin-bottom: 1em;
   color: inherit;
   font-size: inherit;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 100%;
+    
+  }
 `;
