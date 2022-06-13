18ee3ce326622794cb308db4e04b59217cd9b90e
MaylisReynaud <maylis.reynaud74@gmail.com>
2022-06-13 10:18:11 +0200

    vercel

--- CLIENT/src/components/ArticlesPage/Fabric/index.js
+++ CLIENT/src/components/ArticlesPage/Fabric/index.js
@@ -98,7 +98,7 @@ export function Fabric(props, index) {
 
             ))}
           </>
-        ) : <div>Aucun tissu ne correspond à cette sélection </div>}
+        ) : <div>Aucun tissu ne correspond à cette sélection !</div>}
 
       </CardsContainer>
     );
